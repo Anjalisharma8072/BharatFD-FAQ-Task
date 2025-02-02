f4362af79191ead0f3349217de34569146e5395d
Anjalisharma8072 <anjaliiisharma007@gmail.com>
2025-02-02 11:46:35 +0530

    Add multilingual FAQ model

---
+++ faq/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('api/faqs/', views.faq_list, name='faq_list'), 
+    
+]
--- faq/views.py
+++ faq/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import FAQ
+from .serializers import FAQSerializer
+from django.http import JsonResponse
 
-# Create your views here.
+
+@api_view(['GET'])
+def faq_list(request):
+    lang = request.GET.get('lang', 'en') 
+    faqs = FAQ.objects.all()
+
+    faq_data = [faq.get_translated(lang)
+                for faq in faqs] 
+
+    return Response(faq_data, status=status.HTTP_200_OK)
+
